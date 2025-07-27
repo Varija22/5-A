a9027127b36d3a058ace5b478f5a8f82f7b459d4
Varija22 <varija.korti.btech2024@sitpune.edu.in>
2025-07-27 14:39:07 +0530

    Add files via upload

---
+++ Exp5-A.cpp
@@ -0,0 +1,24 @@
+//Varija K
+//24070123165
+//Program 1 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    
+    cout<<"Enter a number:";
+    cin>>num;
+    
+    if(num%2==0){
+        cout<<"Number is EVEN.";
+    }
+    else{
+        cout<<"Number is ODD.";
+    }
+
+    return 0;
+}
+
+
