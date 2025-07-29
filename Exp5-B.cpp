baa385356052ba8c7e1f307824a64267cd5e5b5d
Varija22 <varija.korti.btech2024@sitpune.edu.in>
2025-07-29 14:53:23 +0530

    Add files via upload

---
+++ Exp5-B.cpp
@@ -0,0 +1,24 @@
+//Varija K
+//24070123165
+//Program-2
+#include <iostream>
+using namespace std;
+
+int main() {
+    char ch;
+    
+    cout<<"Enter a Character:";
+    cin>>ch;
+    
+    if(ch== 'a' || ch== 'e' ||ch== 'i' ||ch== 'o' ||ch== 'u' ||ch== 'A' ||ch== 'E' ||ch== 'I' ||ch== 'O' ||ch== 'U'){
+        cout<<"Character is a VOWEL.";
+    }
+    else{
+        cout<<"Character is a CONSONANT.";
+    }
+    
+    return 0;
+}
+
+
+
